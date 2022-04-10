2a4109d58dc8374f22b3e52fccc31f1de23d611c
Eric SHU <shuli0502@gmail.com>
2022-04-10 11:43:34 +0800

    使用RestTemplate进行http远程调用；

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,18 +2,30 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import org.springframework.beans.factory.annotation.Autowired;
+import cn.itcast.order.pojo.User;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @Service
-public class OrderService {
-
-    @Autowired
+public class OrderService
+{
+    @Resource
     private OrderMapper orderMapper;
 
-    public Order queryOrderById(Long orderId) {
+    @Resource
+    private RestTemplate restTemplate;
+
+    public Order queryOrderById(Long orderId)
+    {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+
+        String url = "http://localhost:8081/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
+
         // 4.返回
         return order;
     }
--- user-service/src/main/java/cn/itcast/user/service/UserService.java
+++ user-service/src/main/java/cn/itcast/user/service/UserService.java
@@ -2,16 +2,18 @@ package cn.itcast.user.service;
 
 import cn.itcast.user.mapper.UserMapper;
 import cn.itcast.user.pojo.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import javax.annotation.Resource;
 
-    @Autowired
+@Service
+public class UserService
+{
+    @Resource
     private UserMapper userMapper;
 
-    public User queryById(Long id) {
+    public User queryById(Long id)
+    {
         return userMapper.findById(id);
     }
 }
