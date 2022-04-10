f9ca4fe1a5848d817fb36fdda66225f63cb63184
Eric SHU <shuli0502@gmail.com>
2022-04-10 12:52:05 +0800

    Eureka注册中心：用服务提供者的服务名称远程调用；

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -2,6 +2,7 @@ package cn.itcast.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,8 +17,10 @@ public class OrderApplication
 
     /**
      * 创建RestTemplate并注入Spring容器
+     * 添加负载均衡注解
      */
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate()
     {
         return new RestTemplate();
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -22,7 +22,10 @@ public class OrderService
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
 
-        String url = "http://localhost:8081/user/" + order.getUserId();
+        // String url = "http://localhost:8081/user/" + order.getUserId();
+        // 用加载进eureka的服务名替换主机名和端口
+        String url = "http://user-service/user/" + order.getUserId();
+
         User user = restTemplate.getForObject(url, User.class);
         order.setUser(user);
 
