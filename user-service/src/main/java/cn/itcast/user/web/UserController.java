ab2b0cfb9a15040c6aaf74174713f82955de1c74
Eric SHU <shuli0502@gmail.com>
2022-04-10 20:46:21 +0800

    Nacos配置管理：Server端统一配置管理；

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -3,16 +3,33 @@ package cn.itcast.user.web;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
 @RequestMapping("/user")
-public class UserController {
-
-    @Autowired
+public class UserController
+{
+    @Resource
     private UserService userService;
+
+    @Value("${pattern.dateformat}")
+    private String dateformat;
+
+    @GetMapping("now")
+    public String now()
+    {
+        log.debug("dateformat : {}", dateformat);
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+    }
 
     /**
      * 路径： /user/110
@@ -21,7 +38,8 @@ public class UserController {
      * @return 用户
      */
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
+    public User queryById(@PathVariable("id") Long id)
+    {
         return userService.queryById(id);
     }
 }
