a30fd0fbdf4b32be03090453f3af31c25f108b37
Eric SHU <shuli0502@gmail.com>
2022-04-10 21:03:33 +0800

    Nacos配置管理：配置自动刷新方式二;

--- user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -1,10 +1,9 @@
 package cn.itcast.user.web;
 
+import cn.itcast.user.config.PatternProperties;
 import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,20 +20,25 @@ import java.time.format.DateTimeFormatter;
 @Slf4j
 @RestController
 @RequestMapping("/user")
-@RefreshScope
+// @RefreshScope
 public class UserController
 {
     @Resource
     private UserService userService;
 
-    @Value("${pattern.dateformat}")
-    private String dateformat;
+    // @Value("${pattern.dateformat}")
+    // private String dateformat;
+
+    @Resource
+    private PatternProperties patternProperties;
 
     @GetMapping("now")
     public String now()
     {
-        log.debug("dateformat : {}", dateformat);
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+        // log.debug("dateformat : {}", dateformat);
+        // return LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateformat));
+        log.debug("dateformat : {}", patternProperties.getDateformat());
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(patternProperties.getDateformat()));
     }
 
     /**
