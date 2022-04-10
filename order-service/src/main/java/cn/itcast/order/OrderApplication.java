ad45cc74e3207acb611a4353b73705ace2691ff9
Eric SHU <shuli0502@gmail.com>
2022-04-10 15:42:57 +0800

    Nacos注册中心：集群配置和负载均衡；

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,7 +1,5 @@
 package cn.itcast.order;
 
-import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -31,9 +29,9 @@ public class OrderApplication
     /**
      * 配置当前模块中所有服务采用随机模式负载均衡规则
      */
-    @Bean
+    /*@Bean
     public IRule randomRule()
     {
         return new RandomRule();
-    }
+    }*/
 }
