1036b28724f1a67f22f16c1bdf38b0bc45183ef7
Eric SHU <shuli0502@gmail.com>
2022-04-15 18:30:41 +0800

    RestClient删除和查询；

--- src/test/java/cn/itcast/hotel/HotelIndexTest.java
+++ src/test/java/cn/itcast/hotel/HotelIndexTest.java
@@ -1,10 +1,14 @@
 package cn.itcast.hotel;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,6 +23,7 @@ import java.io.IOException;
  * @date 2022-04-15 16:09
  * @since jdk-11.0.14
  */
+@Slf4j
 public class HotelIndexTest
 {
     private RestHighLevelClient client;
@@ -85,15 +90,39 @@ public class HotelIndexTest
             "  }\n" +
             "}";
 
+    public static final String INDEX_NAME = "hotel";
+
     @Test
     void testCreateHotelIndex() throws IOException
     {
         System.out.println("client = " + client);
         // 1.创建Request对象
-        CreateIndexRequest request = new CreateIndexRequest("hotel");
+        CreateIndexRequest request = new CreateIndexRequest(INDEX_NAME);
         // 2.请求参数，MAPPING_TEMPLATE是静态常量字符串，内容是创建索引库的DSL语句
         request.source(MAPPING_TEMPLATE, XContentType.JSON);
         // 3.发起请求
         client.indices().create(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testIndexCRUD() throws IOException
+    {
+        IndicesClient indices = client.indices();
+
+        if (indices.exists(new GetIndexRequest(INDEX_NAME), RequestOptions.DEFAULT))
+        {
+            log.warn("[{}]索引库已存在，删除索引库。", INDEX_NAME);
+            indices.delete(new DeleteIndexRequest(INDEX_NAME), RequestOptions.DEFAULT);
+        }
+        else
+        {
+            log.warn("[{}]索引库不存在，创建索引库。", INDEX_NAME);
+            // 1.创建Request对象
+            CreateIndexRequest request = new CreateIndexRequest(INDEX_NAME);
+            // 2.请求参数，MAPPING_TEMPLATE是静态常量字符串，内容是创建索引库的DSL语句
+            request.source(MAPPING_TEMPLATE, XContentType.JSON);
+            // 3.发起请求
+            client.indices().create(request, RequestOptions.DEFAULT);
+        }
     }
 }
