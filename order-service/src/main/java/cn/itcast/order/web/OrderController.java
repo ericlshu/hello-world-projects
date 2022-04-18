f75aeb1a02f85daad6efbe4ddb11474a05e5192b
Eric SHU <shuli0502@gmail.com>
2022-04-18 13:39:47 +0800

    微服务保护：流控模式-热点参数限流；

--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -3,6 +3,7 @@ package cn.itcast.order.web;
 import cn.itcast.feign.config.PatternProperties;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.service.OrderService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +17,11 @@ public class OrderController
     @Resource
     private OrderService orderService;
 
+    /**
+     * 热点参数限流对默认的SpringMVC资源无效
+     * 需要添加@SentinelResource注解才能是热点参数限流规则生效
+     */
+    @SentinelResource("hot")
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId, @RequestHeader(value = "header", required = false) String header)
     {
