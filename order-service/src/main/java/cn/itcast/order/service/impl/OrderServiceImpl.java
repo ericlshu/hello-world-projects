452221e3aaf2b8983222085cb9208d4d35c149d7
Eric SHU <shuli0502@gmail.com>
2022-04-19 17:53:46 +0800

    Seata的XA模式;

--- order-service/src/main/java/cn/itcast/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/cn/itcast/order/service/impl/OrderServiceImpl.java
@@ -6,39 +6,48 @@ import cn.itcast.order.entity.Order;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.service.OrderService;
 import feign.FeignException;
+import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author 虎哥
  */
 @Slf4j
 @Service
-public class OrderServiceImpl implements OrderService {
-
+public class OrderServiceImpl implements OrderService
+{
     private final AccountClient accountClient;
     private final StorageClient storageClient;
     private final OrderMapper orderMapper;
 
-    public OrderServiceImpl(AccountClient accountClient, StorageClient storageClient, OrderMapper orderMapper) {
+    public OrderServiceImpl(AccountClient accountClient, StorageClient storageClient, OrderMapper orderMapper)
+    {
         this.accountClient = accountClient;
         this.storageClient = storageClient;
         this.orderMapper = orderMapper;
     }
 
+    /**
+     * 给发起全局事务的入口方法添加@GlobalTransactional注解
+     */
     @Override
-    @Transactional
-    public Long create(Order order) {
+    // @Transactional
+    @GlobalTransactional
+    public Long create(Order order)
+    {
         // 创建订单
         orderMapper.insert(order);
-        try {
+        try
+        {
             // 扣用户余额
             accountClient.deduct(order.getUserId(), order.getMoney());
             // 扣库存
             storageClient.deduct(order.getCommodityCode(), order.getCount());
 
-        } catch (FeignException e) {
+        }
+        catch (FeignException e)
+        {
             log.error("下单失败，原因:{}", e.contentUTF8(), e);
             throw new RuntimeException(e.contentUTF8(), e);
         }
