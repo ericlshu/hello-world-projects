940cf020ad7f5e28660dcbb44593892c9bfe015f
Eric SHU <shuli0502@gmail.com>
2022-04-22 23:22:19 +0800

    OpenResty的Redis模块；

--- src/main/resources/OpenResty/item.lua
+++ src/main/resources/OpenResty/item.lua
@@ -1,14 +1,26 @@
 -- 导入common函数库
 local common = require('common')
 local read_http = common.read_http
+local read_redis = common.read_redis
 -- 导入cjson库
 local cjson = require "cjson"
+
+-- 封装查询函数
+function read_date(key, path, params)
+    local resp = read_redis('127.0.0.1', 6379 , key)
+    if not resp then
+        ngx.log('Redis查询失败，进行http查询, key : ', key)
+        resp = read_http(path,params)
+    end
+    return resp
+end
+
 -- 获取路径参数
 local id = ngx.var[1]
 -- 查询商品信息
-local itemJSON = read_http("/item/" .. id,nil)
+local itemJSON = read_date("item:id:" .. id, "/item/" .. id, nil)
 -- 查询库存信息
-local stockJSON = read_http("/item/stock/" .. id,nil)
+local stockJSON = read_date("item:stock:id:" .. id, "/item/stock/" .. id, nil)
 -- JSON转化为lua的table
 local item = cjson.decode(itemJSON)
 local stock = cjson.decode(stockJSON)
