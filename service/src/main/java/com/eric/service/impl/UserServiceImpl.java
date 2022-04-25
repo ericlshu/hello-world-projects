8d85d9286b10005550d7a1c6ff33b4dceb3c5a44
Eric SHU <shuli0502@gmail.com>
2022-04-25 22:06:10 +0800

    服务提供方dubbo改造；

--- service/src/main/java/com/eric/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/eric/service/impl/UserServiceImpl.java
@@ -1,16 +1,18 @@
 package com.eric.service.impl;
 
 import com.eric.service.UserService;
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.Service;
 
 /**
  * Description :
+ * DubboService注解将本类提供的服务对外发布，将访问的地址，ip以及路径注册到注册中心
  *
  * @author Eric L SHU
  * @date 2022-04-24 14:18
  * @since jdk-11.0.14
  */
-@Service()
+// @Service()
+@Service
 public class UserServiceImpl implements UserService
 {
     @Override
