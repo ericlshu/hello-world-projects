d8466cef411f5f770e969abf45a854c5a81c7856
Eric SHU <shuli0502@gmail.com>
2022-05-01 09:13:59 +0800

    RabbitMQ Confirm Callback;

--- spring-producer/src/test/java/com/eric/ProducerTest.java
+++ spring-producer/src/test/java/com/eric/ProducerTest.java
@@ -2,6 +2,7 @@ package com.eric;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -43,5 +44,35 @@ public class ProducerTest
         rabbitTemplate.convertAndSend("topic_exchange",
                                       "eric.test",
                                       "Hello, Topic类型交换机消息*通配符；");
+    }
+
+    /**
+     * 确认模式
+     * 步骤：
+     * // 1. 确认模式开启：ConnectionFactory中开启publisher-confirms="true"
+     * 1. 确认模式开启：ConnectionFactory中开启confirm-type="CORRELATED"
+     * 2. 在rabbitTemplate定义ConfirmCallBack回调函数
+     */
+    @Test
+    public void testConfirm()
+    {
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback()
+        {
+            /**
+             * @param correlationData correlation data for the callback.
+             * @param ack true for ack, false for nack
+             * @param cause An optional cause, for nack, when available, otherwise null.
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause)
+            {
+                System.out.println("org.springframework.amqp.rabbit.core.RabbitTemplate.ConfirmCallback.confirm ......");
+                if (ack)
+                    System.out.println("消息接收成功：" + cause);
+                else
+                    System.out.println("消息接收失败：" + cause);
+            }
+        });
+        rabbitTemplate.convertAndSend("confirm_exchange", "confirm", "Helle, confirm queue!");
     }
 }
