6b41352c3dd7a9ecade83322591473f7b7328a07
Eric SHU <shuli0502@gmail.com>
2022-05-07 20:50:20 +0800

    屏蔽session中敏感信息；

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
@@ -54,13 +56,13 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             return Result.fail("手机号格式错误！");
         }
         // 3.验证码校验
-        /*Object sessionCode = session.getAttribute("code");
+        Object sessionCode = session.getAttribute("code");
         String inputCode = loginForm.getCode();
         if (sessionCode == null || !sessionCode.toString().equals(inputCode))
         {
             // 4.1.不一致，报错
             return Result.fail("验证码错误！");
-        }*/
+        }
         // 4.2.一致，根据手机号查询用户
         User user = query().eq("phone", phone).one();
         // 5.判断用户是否存在
@@ -70,7 +72,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             user = createUserWithPhone(phone);
         }
         // 7.保存用户信息到session中
-        session.setAttribute("user", user);
+        // session.setAttribute("user", user);
+        // 隐藏敏感信息
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        session.setAttribute("user", userDTO);
         // 7.1.随机生成token，作为登录令牌
         // 7.2.将User对象转为HashMap存储
         // 7.3.存储
--- hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ hm-dianping/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package com.hmdp.utils;
 
-import com.hmdp.entity.User;
+import com.hmdp.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -22,8 +22,7 @@ public class LoginInterceptor implements HandlerInterceptor
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
     {
         log.info("用户请求拦截器");
-        log.debug("pathInfo : {}", request.getPathInfo());
-        User user = (User) request.getSession().getAttribute("user");
+        UserDTO user = (UserDTO) request.getSession().getAttribute("user");
         log.debug("user : {}", user);
         if (user == null)
         {
