45c47f44212af9389361fdf93d6c522303fb8b59
Eric SHU <shuli0502@gmail.com>
2022-05-07 18:08:19 +0800

    短信验证码登录;

--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -39,5 +41,51 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("发送短信验证码成功,验证码:{}", code);
         // 返回ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session)
+    {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone))
+        {
+            // 2.不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.验证码校验
+        Object sessionCode = session.getAttribute("code");
+        String inputCode = loginForm.getCode();
+        if (sessionCode == null || !sessionCode.toString().equals(inputCode))
+        {
+            // 4.1.不一致，报错
+            return Result.fail("验证码错误！");
+        }
+        // 4.2.一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+        // 5.判断用户是否存在
+        if (user == null)
+        {
+            // 6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+        // 7.保存用户信息到session中
+        session.setAttribute("user", user);
+        // 7.1.随机生成token，作为登录令牌
+        // 7.2.将User对象转为HashMap存储
+        // 7.3.存储
+        // 7.4.设置token有效期
+        // 8.返回token
+
+        return null;
+    }
+
+    private User createUserWithPhone(String phone)
+    {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
     }
 }
