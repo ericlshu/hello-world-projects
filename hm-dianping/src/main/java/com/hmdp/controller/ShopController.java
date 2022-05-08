188f5530f9cb7c7e451bf699b496c7ecd1e76afd
Eric SHU <shuli0502@gmail.com>
2022-05-08 10:53:42 +0800

    根据id查询商铺添加缓存；

--- hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/ShopController.java
@@ -1,48 +1,47 @@
 package com.hmdp.controller;
 
-
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.SystemConstants;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
+@Slf4j
 @RestController
 @RequestMapping("/shop")
-public class ShopController {
-
+public class ShopController
+{
     @Resource
     public IShopService shopService;
 
     /**
      * 根据id查询商铺信息
+     *
      * @param id 商铺id
      * @return 商铺详情数据
      */
     @GetMapping("/{id}")
-    public Result queryShopById(@PathVariable("id") Long id) {
-        return Result.ok(shopService.getById(id));
+    public Result queryShopById(@PathVariable("id") Long id)
+    {
+        log.info("根据id查询商铺信息");
+        // return Result.ok(shopService.getById(id));
+        return shopService.queryById(id);
     }
 
     /**
      * 新增商铺信息
+     *
      * @param shop 商铺数据
      * @return 商铺id
      */
     @PostMapping
-    public Result saveShop(@RequestBody Shop shop) {
+    public Result saveShop(@RequestBody Shop shop)
+    {
         // 写入数据库
         shopService.save(shop);
         // 返回店铺id
@@ -51,11 +50,13 @@ public class ShopController {
 
     /**
      * 更新商铺信息
+     *
      * @param shop 商铺数据
      * @return 无
      */
     @PutMapping
-    public Result updateShop(@RequestBody Shop shop) {
+    public Result updateShop(@RequestBody Shop shop)
+    {
         // 写入数据库
         shopService.updateById(shop);
         return Result.ok();
@@ -63,7 +64,8 @@ public class ShopController {
 
     /**
      * 根据商铺类型分页查询商铺信息
-     * @param typeId 商铺类型
+     *
+     * @param typeId  商铺类型
      * @param current 页码
      * @return 商铺列表
      */
@@ -71,7 +73,8 @@ public class ShopController {
     public Result queryShopByType(
             @RequestParam("typeId") Integer typeId,
             @RequestParam(value = "current", defaultValue = "1") Integer current
-    ) {
+    )
+    {
         // 根据类型分页查询
         Page<Shop> page = shopService.query()
                 .eq("type_id", typeId)
@@ -82,7 +85,8 @@ public class ShopController {
 
     /**
      * 根据商铺名称关键字分页查询商铺信息
-     * @param name 商铺名称关键字
+     *
+     * @param name    商铺名称关键字
      * @param current 页码
      * @return 商铺列表
      */
@@ -90,7 +94,8 @@ public class ShopController {
     public Result queryShopByName(
             @RequestParam(value = "name", required = false) String name,
             @RequestParam(value = "current", defaultValue = "1") Integer current
-    ) {
+    )
+    {
         // 根据类型分页查询
         Page<Shop> page = shopService.query()
                 .like(StrUtil.isNotBlank(name), "name", name)
--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,20 +1,51 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
-/**
- * <p>
- *  服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
-@Service
-public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+import javax.annotation.Resource;
 
+@Slf4j
+@Service
+public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService
+{
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id)
+    {
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+
+        // 1 从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+        log.debug("shopJson : {}", shopJson);
+
+        // 2.1 存在则返回
+        if (!StrUtil.isBlank(shopJson))
+        {
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        // 2.2 不存在则根据id查询db
+        Shop shop = getById(id);
+        log.debug("shop : {}", shop);
+
+        // 3.1 不存在则返回异常
+        if (shop == null)
+            return Result.fail("店铺不存在，请重试！");
+        // 3.2 存在则返回数据
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+
+        return Result.ok(shop);
+    }
 }
