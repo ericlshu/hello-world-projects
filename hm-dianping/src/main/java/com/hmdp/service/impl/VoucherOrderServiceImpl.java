05091607322afafb5c32eb3e2dcdd8e3a645d698
Eric SHU <shuli0502@gmail.com>
2022-05-10 22:39:55 +0800

    实现一人一单，并解决单机线程安全问题；

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -25,7 +25,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     private RedisIdWorker redisIdWorker;
 
     @Override
-    @Transactional
+
     public Result secKill(Long voucherId)
     {
         // 1. 查询优惠券
@@ -44,27 +44,45 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         if (voucher.getStock() < 1)
             return Result.fail("优惠券已抢完！");
 
-        // 5. 扣减库存
-        boolean success = secKillVoucherService.update()
-                .setSql("stock = stock - 1")
-                .eq("voucher_id", voucherId)
-                // .eq("stock", voucher.getStock())    // CAS乐观锁解决超卖问题，但会造成失败率降低的问题
-                .gt("stock", 0)
-                .update();
-        if (!success)
-            return Result.fail("优惠券已抢完！");
+        return creatVoucherOrder(voucherId);
+    }
 
-        // 6. 创建订单
-        long orderId = redisIdWorker.nextId("order");
+    @Transactional
+    public Result creatVoucherOrder(Long voucherId)
+    {
+        // 添加一人一单校验功能
+        Long userId = UserHolder.getUser().getId();
+        // 对方法加锁相当于对当前类加锁，并发场景下为串行执行
+        // 对当前用户id加锁，保证不同的user使用同一事务
+        // 使用intern()方法将字符串放入常量池，保证同一用户取到同一字符串对象
+        synchronized (userId.toString().intern())
+        {
+            Long count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+            if (count > 0)
+                return Result.fail("当前用户已经购买过优惠券！");
 
-        VoucherOrder voucherOrder = new VoucherOrder();
-        voucherOrder.setId(orderId);
-        voucherOrder.setVoucherId(orderId);
-        voucherOrder.setUserId(UserHolder.getUser().getId());
-        voucherOrder.setVoucherId(voucherId);
-        save(voucherOrder);
+            // 5. 扣减库存
+            boolean success = secKillVoucherService.update()
+                    .setSql("stock = stock - 1")
+                    .eq("voucher_id", voucherId)
+                    // .eq("stock", voucher.getStock())    // CAS乐观锁解决超卖问题，但会造成失败率降低的问题
+                    .gt("stock", 0)
+                    .update();
+            if (!success)
+                return Result.fail("优惠券已抢完！");
 
-        // 7. 返回结果
-        return Result.ok(orderId);
+            // 6. 创建订单
+            long orderId = redisIdWorker.nextId("order");
+
+            VoucherOrder voucherOrder = new VoucherOrder();
+            voucherOrder.setId(orderId);
+            voucherOrder.setVoucherId(orderId);
+            voucherOrder.setUserId(userId);
+            voucherOrder.setVoucherId(voucherId);
+            save(voucherOrder);
+
+            // 7. 返回结果
+            return Result.ok(orderId);
+        }
     }
 }
