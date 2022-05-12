2a30cd85df815f0365194144340bd1577405ca5b
Eric SHU <shuli0502@gmail.com>
2022-05-12 22:08:09 +0800

    使用Lua脚本释放分布式锁；

--- hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ hm-dianping/src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,10 +16,20 @@ import java.util.concurrent.TimeUnit;
  * @date 2022-05-10 23:19
  * @since jdk-11.0.14
  */
+@Slf4j
 public class SimpleRedisLock implements Lock
 {
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static
+    {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("lua/unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     private final String name;
     private final StringRedisTemplate stringRedisTemplate;
@@ -30,13 +44,22 @@ public class SimpleRedisLock implements Lock
     public boolean tryLock(long timeoutSec)
     {
         String threadId = ID_PREFIX + Thread.currentThread().getId();
-        System.out.println("threadId = " + threadId);
+        log.info("threadId = " + threadId);
         Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(
                 KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(result);
     }
 
-    @Override
+    public void unlock()
+    {
+        // 调用lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+    /*@Override
     public void unlock()
     {
         // 获取线程标识
@@ -47,5 +70,5 @@ public class SimpleRedisLock implements Lock
         if (threadId.equals(id))
             // 释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
-    }
+    }*/
 }
