f6b5428860a47fa22949a9662205b7313af525d2
Eric SHU <shuli0502@gmail.com>
2022-05-15 12:49:58 +0800

    发布并查询探店笔记功能；

--- hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
@@ -4,9 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
-import com.hmdp.entity.User;
 import com.hmdp.service.IBlogService;
-import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import org.springframework.web.bind.annotation.*;
@@ -14,22 +12,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @RestController
 @RequestMapping("/blog")
 public class BlogController
 {
     @Resource
     private IBlogService blogService;
-    @Resource
-    private IUserService userService;
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog)
@@ -68,20 +56,12 @@ public class BlogController
     @GetMapping("/hot")
     public Result queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current)
     {
-        // 根据用户查询
-        Page<Blog> page = blogService.query()
-                .orderByDesc("liked")
-                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
-        // 获取当前页数据
-        List<Blog> records = page.getRecords();
-        // 查询用户
-        records.forEach(blog ->
-                        {
-                            Long userId = blog.getUserId();
-                            User user = userService.getById(userId);
-                            blog.setName(user.getNickName());
-                            blog.setIcon(user.getIcon());
-                        });
-        return Result.ok(records);
+        return blogService.queryHotBlogs(current);
+    }
+
+    @GetMapping("/{id}")
+    public Result queryBolgById(@PathVariable Long id)
+    {
+        return blogService.queryBolgById(id);
     }
 }
--- hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
@@ -1,16 +1,11 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
-public interface IBlogService extends IService<Blog> {
-
+public interface IBlogService extends IService<Blog>
+{
+    Result queryBolgById(Long id);
+    Result queryHotBlogs(Integer current);
 }
