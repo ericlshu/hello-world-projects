7b1fa45e9f51936864a82e552055231a83723cc8
Eric SHU <shuli0502@gmail.com>
2022-06-08 21:43:32 +0800

    Java客户端简单获取数据;

---
+++ src/test/java/com/eric/es/ElasticSearchTest.java
@@ -0,0 +1,46 @@
+package com.eric.es;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Description :
+ *
+ * @author Eric SHU
+ */
+public class ElasticSearchTest
+{
+    @Test
+    public void test() throws IOException
+    {
+        // 获取客户端连接
+        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(
+                new HttpHost(
+                        "110.40.224.64",
+                        9200,
+                        HttpHost.DEFAULT_SCHEME_NAME)
+        ));
+        // 构建请求
+        GetRequest getRequest = new GetRequest("book", "2");
+
+        // 执行
+        GetResponse response = client.get(getRequest, RequestOptions.DEFAULT);
+
+        // 获取结果
+        String id = response.getId();
+        long version = response.getVersion();
+        Map<String, Object> source = response.getSource();
+
+        System.out.println("id = " + id);
+        System.out.println("version = " + version);
+        System.out.println("source = " + source);
+    }
+}
